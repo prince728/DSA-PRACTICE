6c1aa4747704f27ef86420a06e88ef6a98c1c235
Prince Gupta <78577947+prince728@users.noreply.github.com>
2026-06-29 13:38:48 +0530

    Time: 0 ms (100%), Space: 18.8 MB (5.28%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,18 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if (root == nullptr) return true;
-        return isMirror(root->left, root->right);
-    }
+        queue<TreeNode*>q;
+        if(root==nullptr) return 0;
 
-    bool isMirror(TreeNode* t1, TreeNode* t2) {
-    
-        if (t1 == nullptr && t2 == nullptr) return true;
-        if (t1 == nullptr || t2 == nullptr) return false;
-        
-        
-        return (t1->val == t2->val) 
-            && isMirror(t1->left, t2->right) 
-            && isMirror(t1->right, t2->left);
+        if((!root->left&&root->right) ||  (root->left&&!root->right)) return 0;
+        q.push(root->left);
+        q.push(root->right);
+
+        while(!q.empty()){
+            TreeNode* t1=q.front();
+            q.pop();
+            TreeNode* t2=q.front();
+            q.pop();
+
+            if(t1==nullptr&& t2==nullptr) continue;
+             if((t1&&!t2) || (!t1&&t2)) return 0;
+
+            if(t1->val!=t2->val) return 0;
+
+            q.push(t1->left),q.push(t2->right),q.push(t1->right),q.push(t2->left);
+        }
+        return 1;
+
     }
 };
