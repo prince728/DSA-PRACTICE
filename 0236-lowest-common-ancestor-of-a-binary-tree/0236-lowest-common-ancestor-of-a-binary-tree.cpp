e543bd1407c7e39916eb36c2b416b42dfba5fdf8
Prince Gupta <78577947+prince728@users.noreply.github.com>
2026-07-26 18:24:01 +0530

    Time: 65 ms (42.36%), Space: 43.9 MB (46.9%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,28 +9,24 @@
  */
 class Solution {
 public:
-    bool isPresent(TreeNode* root, int& val1, int& val2) {
-        if (!root)
-            return 0;
-        if (root->val == val1 || root->val == val2)
-            return 1;
-
-        return isPresent(root->left, val1, val2) ||
-               isPresent(root->right, val1, val2);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        while (root) {
-            if(root->val== p->val  || root->val ==q->val) return root;
-            else if (isPresent(root->left, p->val, q->val) &&
-                isPresent(root->right, p->val, q->val))
-                return root;
-            else if (!isPresent(root->left, p->val, q->val)){
-                root = root->right;
-            }
-            else if (!isPresent(root->right, p->val, q->val)){
-                root = root->left;
-            }
-        }
-        return nullptr;
+        if(!root || root==p || root ==q) return root;
+
+        TreeNode*leftLCA = lowestCommonAncestor(root->left,p,q);
+        TreeNode*rightLCA = lowestCommonAncestor(root->right,p,q);
+
+        if(leftLCA && rightLCA) return root;
+
+        return leftLCA ? leftLCA : rightLCA;
+
     }
 };
+
+
+
+
+
+        
+
+     
+
