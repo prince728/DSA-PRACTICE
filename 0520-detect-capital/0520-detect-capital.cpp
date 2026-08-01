e29d270c7b552969555513b8c01a85ce6950211f
Prince Gupta <78577947+prince728@users.noreply.github.com>
2026-08-01 14:18:31 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.07%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count = 0;
+        for (char c : word) {
+            if (c >= 'A' && c <= 'Z') count++;
+        }
+        if (count == word.size() || count == 0) return true;
+        if (count == 1 && (word[0] >= 'A' && word[0] <= 'Z')) return true;
+        
+        return false;
+    }
+};
