1e2c05d4ee0790161e93379e5f258e0aba418a04
Prince Gupta <78577947+prince728@users.noreply.github.com>
2026-09-17 12:17:29 +0530

    Time: 23 ms (18.57%), Space: 38.6 MB (26.38%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int sum=0,ans=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.count(sum-goal)) ans+=mp[sum-goal];
+            mp[sum]++;
+            
+        }
+
+        return ans;
+    }
+};
